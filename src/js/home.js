1fda91e10cf78074a9feff5e281d70cdda8a444b
coffeecoke <18601376034@163.com>
2019-03-25 19:56:30 +0800

    20190325

--- src/js/home.js
+++ src/js/home.js
@@ -1,5 +1,12 @@
 $('#roll').rollSlide({
-  orientation: 'right',
+  orientation: 'left',
+  num: 2,
+  v: 1500,
+  space: 3000,
+  isRoll: true
+});
+$('#roll2').rollSlide({
+  orientation: 'left',
   num: 2,
   v: 1500,
   space: 3000,
